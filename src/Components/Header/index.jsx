9822abdcb3dc28dabf71b3f9fbc95c98134e19d7
Bruno Guimaraes <devgmrs@gmail.com>
2022-10-24 19:26:30 -0300

    :sparkles:	feat: Add Componente Header

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,9 +1,32 @@
-import './index.css';
+import React from 'react';
+import styles from './header.module.scss';
 
+//TODO Fazer layout cabecalho e testes(se necessário) de acordo com o design do figma
 export const Header = () => {
   return (
     <header>
-      <div className="header_background"></div>
+      <div className={styles.header_background}>
+        <div className={styles.logotipo}>
+          <picture>
+            <source
+              srcset="/imagens/logo-1024w.png"
+              media="(min-width: 1024px)"
+            />
+            <img
+              src="/imagens/logo-pequeno.png"
+              className={styles.logo}
+              alt="Logotipo Amigo Secreto"
+            />
+          </picture>
+        </div>
+        <div>
+          <img
+            src="/imagens/WavyStock.png"
+            className={styles.logotipo_imagem}
+            alt="Desenho 2d de mulher segurando um planner"
+          />
+        </div>
+      </div>
     </header>
   );
 };
