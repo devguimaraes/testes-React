329a9f7073d48629956b50e6416adef0c9171cc2
Bruno Guimaraes <devgmrs@gmail.com>
2022-10-24 19:25:11 -0300

    :lipstick:	feat: Adicionando SCSS modules ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Formulario from './Components/Formulario';
 import { Header } from './Components/Header';
-import './index.css';
+import './index.module.scss';
 
 const App = () => {
   return (
